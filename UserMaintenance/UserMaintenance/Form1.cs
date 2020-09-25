c9156087d1bd615546764f6b61e92a974c61508a
Katalin Vörös <katalin.voros2@stud.uni-corvinus.hu>
2020-09-25 19:45:53 +0200

    Add function commit

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -7,11 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UserMaintenance.Entities;
 
 namespace UserMaintenance
 {
     public partial class Form1 : Form
     {
+        BindingList<User> users = new BindingList<User>();
         public Form1()
         {
             InitializeComponent();
@@ -19,6 +21,21 @@ namespace UserMaintenance
             label1.Text = Resource.String1;
             label2.Text = Resource.String2;
             button1.Text = Resource.String3;
+
+            listBox1.DataSource = users;
+            listBox1.ValueMember = "ID";
+            listBox1.DisplayMember = "FullName";
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            var u = new User()
+            {
+                LastName = textBox1.Text,
+                FirstName = textBox2.Text
+            };
+
+            users.Add(u);
         }
     }
 }
