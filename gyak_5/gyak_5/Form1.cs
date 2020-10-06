ecfff7627ef844ad7131bb830c4e739f1fcef970
Katalin Vörös <katalin.voros2@stud.uni-corvinus.hu>
2020-10-06 20:50:24 +0200

    Create Portfolio

--- gyak_5/gyak_5/Form1.cs
+++ gyak_5/gyak_5/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using gyak_5.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,30 @@ namespace gyak_5
 {
     public partial class Form1 : Form
     {
+        PortfolioEntities context = new PortfolioEntities();
+        List<Tick> Ticks;
+
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
+
         public Form1()
         {
             InitializeComponent();
+
+            Ticks = context.Ticks.ToList();
+            dataGridView1.DataSource = Ticks;
+
+            CreatePortfolio();
         }
+
+        private void CreatePortfolio()
+        {
+            Portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+
+            dataGridView2.DataSource = Portfolio;
+        }
+
+       
     }
 }
