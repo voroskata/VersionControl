a2fb9c7eb13ba2db55b6667bf77c3b83bbae4bd4
Katalin Vörös <katalin.voros2@stud.uni-corvinus.hu>
2020-11-10 22:31:10 +0100

    Car

---
+++ gyak_8/gyak_8/Entities/Car.cs
@@ -0,0 +1,19 @@
+﻿using gyak_8.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gyak_8.Entities
+{
+    class Car : Toy
+    {
+        protected override void DrawImage(Graphics g)
+        {
+            Image imageFile = Image.FromFile("Images/car.png");
+            g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
+        }
+    }
+}
