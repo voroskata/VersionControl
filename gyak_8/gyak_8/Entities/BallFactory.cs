87d15e14f6278e353da0017881de6ee1753c0903
Katalin Vörös <katalin.voros2@stud.uni-corvinus.hu>
2020-11-10 22:01:34 +0100

    Conveyor

--- gyak_8/gyak_8/Entities/BallFactory.cs
+++ gyak_8/gyak_8/Entities/BallFactory.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace gyak_8.Entities
 {
-    class BallFactory
+    public class BallFactory
     {
         public Ball CreateNew()
         {
--- gyak_8/gyak_8/Form1.cs
+++ gyak_8/gyak_8/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using gyak_8.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,46 @@ namespace gyak_8
 {
     public partial class Form1 : Form
     {
+        private List<Ball> _balls = new List<Ball>();
+
+        private BallFactory _factory;
+        public BallFactory Factory
+        {
+            get { return _factory; }
+            set { _factory = value; }
+        }
+
         public Form1()
         {
             InitializeComponent();
+            Factory = new BallFactory();
+        }
+
+        private void ConveyorTimer_Tick(object sender, EventArgs e)
+        {
+            var maxPosition = 0;
+
+            foreach (var ball in _balls)
+            {
+                ball.MoveBall();
+                if (ball.Left > maxPosition)
+                    maxPosition = ball.Left;
+            }
+
+            if (maxPosition > 1000)
+            {
+                var oldestBall = _balls[0];
+                mainPanel.Controls.Remove(oldestBall);
+                _balls.Remove(oldestBall);
+            }
+        }
+
+        private void CreateTimer_Tick(object sender, EventArgs e)
+        {
+            var ball = Factory.CreateNew();
+            _balls.Add(ball);
+            ball.Left = -ball.Width;
+            mainPanel.Controls.Add(ball);
         }
     }
 }
