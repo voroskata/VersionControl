dcf1178c01393288ead86677fd9c787c4e4afea7
Katalin Vörös <katalin.voros2@stud.uni-corvinus.hu>
2020-11-26 08:30:21 +0100

    new generation

--- gyak_10/gyak_10/Form1.cs
+++ gyak_10/gyak_10/Form1.cs
@@ -31,12 +31,27 @@ namespace gyak_10
             //gc.AddPlayer();
             //gc.Start(true);
 
+            gc.GameOver += Gc_GameOver;
+
             for (int i = 0; i < populationSize; i++)
             {
                 gc.AddPlayer(nbrOfSteps);
             }
 
             gc.Start(true);
+        }
+
+        private void Gc_GameOver(object sender)
+        {
+            generation++;
+            label1.Text = string.Format(
+            "{0}. generáció",
+            generation);
+
+            var playerList = from p in gc.GetCurrentPlayers()
+                             orderby p.GetFitness() descending
+                             select p;
+            var topPerformers = playerList.Take(populationSize / 2).ToList();
         }
     }
 }
